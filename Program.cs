44b71ad26bd37035c010f7cd8cb582ba53b5f421
Fernando Seguim <fernando.seguim@gmail.com>
2018-06-19 10:12:17 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace deamon_netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sleep = 3000;
+            if (args.Length > 0) { int.TryParse(args[0], out sleep); }
+            while (true)
+            {
+                Console.WriteLine($"Working, pausing for {sleep} ms");
+                Thread.Sleep(sleep);
+            }
+        }
+    }
+}
